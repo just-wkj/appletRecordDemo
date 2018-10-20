0273367abefbe30ecd513d7d46e54e3fc1eee83a
wangkeji <justwkj@gmail.com>
2018-10-20 22:55:05 +0800

    录音时长添加

--- pages/newMusic/index.js
+++ pages/newMusic/index.js
@@ -1,135 +1,118 @@
 // pages/newMusic/index.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
-  },
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        src: '', //录音文件
+        time: 0,//录音时长
+        startTime: 0,//录音开始时间
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var that = this;
-    this.recorderManager = wx.getRecorderManager();
-    this.recorderManager.onError(function(){
-      that.tip("录音失败！")
-    });
-    this.recorderManager.onStop(function(res){
-      that.setData({
-        src: res.tempFilePath 
-      })
-      console.log(res.tempFilePath )
-      that.tip("录音完成！")
-    });
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+        var that = this;
+        this.recorderManager = wx.getRecorderManager();
+        this.recorderManager.onError(function () {
+            that.tip("录音失败！")
+        });
+        this.recorderManager.onStop(function (res) {
+            console.log(res.tempFilePath)
+            that.setData({
+                src: res.tempFilePath,
+            })
+        });
 
-    this.innerAudioContext = wx.createInnerAudioContext();
-    this.innerAudioContext.onError((res) => {
-      that.tip("播放录音11失败！")
-    })
+        this.innerAudioContext = wx.createInnerAudioContext();
+        this.innerAudioContext.onError((res) => {
+            that.tip("播放录音失败！")
+        })
 
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function () {
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
+    },
 
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function () {
+        this.setData({
+            startTime: 0,
+        })
+    },
 
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
 
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
+    /**
+     * 提示
+     */
+    tip: function (msg) {
+        wx.showModal({
+            title: '提示',
+            content: msg,
+            showCancel: false
+        })
+    },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
+    /**
+     * 录制aac音频
+     */
+    startRecordAac: function () {
+        this.recorderManager.start({
+            format: 'aac'
+        });
+    },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
+    /**
+     * 录制mp3音频
+     */
+    startRecordMp3: function () {
+        console.log('开始录音')
+        this.setData({
+            src: '',
+            startTime: this.getCurTime()
+        })
+        this.recorderManager.start({
+            format: 'mp3'
+        });
+    },
 
-  /**
-  * 提示
-  */
-  , tip: function (msg) {
-    wx.showModal({
-      title: '提示',
-      content: msg,
-      showCancel: false
-    })
-  }
+    /**
+     * 停止录音
+     */
+    stopRecord: function () {
+        console.log('结束录音')
+        this.setData({
+            time: (this.getCurTime() - this.data.startTime) / 1000 //计算录音时长
+        })
+        this.recorderManager.stop()
+    },
 
-  /**
-   * 录制aac音频
-   */
-  ,startRecordAac: function(){
-    this.recorderManager.start({
-      format: 'aac'
-    });
-  }
-
-  /**
-   * 录制mp3音频
-  */
-  , startRecordMp3: function () {
-    this.recorderManager.start({
-      format: 'mp3'
-    });
-  }
-
-  /**
-   * 停止录音
-   */
-  ,stopRecord: function(){
-    this.recorderManager.stop()
-  }
-
-  /**
-   * 播放录音
-   */
-  , playRecord: function(){
-    var that = this;
-    var src = this.data.src;
-    if (src == '') {
-      this.tip("请先录音!！")
-      return;
+    /**
+     * 播放录音
+     */
+    playRecord: function () {
+        console.log('点击播放')
+        console.log(this.data)
+        var src = this.data.src;
+        if (src == '') {
+            this.tip("请先录音!！")
+            return;
+        }
+        console.log(this.data.src)
+        this.innerAudioContext.src = this.data.src;
+        this.innerAudioContext.play()
+    },
+    getCurTime() {
+        return new Date().getTime()
     }
-    console.log(this.data.src)
-    this.innerAudioContext.src = this.data.src;
-    this.innerAudioContext.play()
-  }
+
 
 })
